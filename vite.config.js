e1ffdd821bda44c66ce39299b77979420febe657
daromeroji1989-droid <daromeroji1989@gmail.com>
2026-02-09 01:21:45 +0100

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: './', // Especifica explícitamente la raíz del proyecto
   plugins: [react()],
   base: './', // Añadido para resolver rutas relativas en Docker
   build: {
