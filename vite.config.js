da523d20c25fea9772664f54675e9812e61bcfc6
daromeroji1989-droid <daromeroji1989@gmail.com>
2026-02-09 01:45:08 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: './', // Especifica explícitamente la raíz del proyecto
-  plugins: [react()],
-  base: './', // Añadido para resolver rutas relativas en Docker
-  build: {
-    rollupOptions: {
-      input: 'index.html',
-    },
-  },
+    plugins: [react()],
+  base: '/jukebox_base',
+  
 })
